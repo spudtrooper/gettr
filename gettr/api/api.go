6a171c0289eaa24f8d73e02df81e61109b0dc591
Jeff Palm <jeff@jeffpalm.com>
2022-01-07 08:55:16 -0500

    refactor

--- gettr/api/api.go
+++ gettr/api/api.go
@@ -1,30 +1,11 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 
 	"github.com/pkg/errors"
 	"github.com/spudtrooper/goutil/or"
 )
-
-type Client struct {
-	xAppAuth string
-	debug    bool
-}
-
-func MakeClient(user, token string, mOpts ...MakeClientOption) *Client {
-	opts := MakeMakeClientOptions(mOpts...)
-	xAppAuth := fmt.Sprintf(`{"user": "%s", "token": "%s"}`, user, token)
-	return &Client{
-		xAppAuth: xAppAuth,
-		debug:    opts.Debug(),
-	}
-}
 
 type UserInfo struct {
 	Nickname string
@@ -105,56 +86,4 @@ func (c *Client) GetSuggestions(sOpts ...SuggestOption) (*Suggestions, error) {
 		res.HTInfo = append(res.HTInfo, s)
 	}
 	return &res, nil
-}
-
-func (c *Client) request(route string, result interface{}) error {
-	url := fmt.Sprintf("https://api.gettr.com/%s", route)
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Set("x-app-auth", c.xAppAuth)
-	if err != nil {
-		return err
-	}
-	doRes, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	data, err := ioutil.ReadAll(doRes.Body)
-	if err != nil {
-		return err
-	}
-
-	doRes.Body.Close()
-
-	if c.debug {
-		prettyJSON, err := prettyPrintJSON(data)
-		if err != nil {
-			return err
-		}
-		log.Printf("from route %q have response %s", route, prettyJSON)
-	}
-
-	var payload struct {
-		ResponseCode string `json:"rc"`
-		Result       interface{}
-	}
-	payload.Result = result
-	if err := json.Unmarshal(data, &payload); err != nil {
-		return err
-	}
-
-	if payload.ResponseCode != "OK" {
-		return errors.Errorf("non-OK response: %+v", payload)
-	}
-
-	return nil
-}
-
-func prettyPrintJSON(b []byte) (string, error) {
-	var prettyJSON bytes.Buffer
-	if err := json.Indent(&prettyJSON, b, "", "\t"); err != nil {
-		return "", err
-	}
-	return prettyJSON.String(), nil
 }
