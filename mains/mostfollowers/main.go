61e8513ec216478f9804671ae69eab8ee85a4e9f
Jeff Palm <jeff@jeffpalm.com>
2022-01-18 08:30:44 -0500

    update Tue Jan 18 08:30:44 EST 2022

---
+++ mains/mostfollowers/main.go
@@ -0,0 +1,62 @@
+package mostfollowers
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/spudtrooper/gettr/model"
+	"github.com/spudtrooper/goutil/check"
+)
+
+var (
+	threads = flag.Int("threads", 500, "number of threads to process user dirs")
+)
+
+func Main(ctx context.Context) {
+	factory, err := model.MakeFactoryFromFlags(ctx)
+	check.Err(err)
+
+	keys, errs, err := factory.Cache().FindKeysChannels("users")
+	check.Err(err)
+
+	type follower struct {
+		username  string
+		followers int
+	}
+	followers := make(chan follower)
+	go func() {
+		var wg sync.WaitGroup
+		for i := 0; i < *threads; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for k := range keys {
+					u := factory.MakeUser(k)
+					f, err := u.GetFollowers(ctx)
+					if err != nil {
+						log.Printf("GetFollowers: %v", err)
+						continue
+					}
+					followers <- follower{username: k, followers: f}
+				}
+				for e := range errs {
+					log.Printf("ignoring error: %v", e)
+				}
+			}()
+		}
+		wg.Wait()
+		close(followers)
+	}()
+
+	var max follower
+	for f := range followers {
+		if f.followers > max.followers {
+			max = f
+			log.Printf("new max: %+v", max)
+		}
+	}
+	fmt.Printf("max: %+v\n", max)
+}
