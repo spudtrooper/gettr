d6525e6b2d6bde1f128bf375b5e70d3cdbc6c5f4
Jeff Palm <jeff@jeffpalm.com>
2022-01-18 08:29:31 -0500

    update Tue Jan 18 08:29:31 EST 2022

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,11 +6,11 @@ dir=$(dirname $0)
 
 $dir/unit_tests.sh
 
-for f in mains/*.go; do
+for f in $(find mains -name '*.go' | xargs); do
     echo "==      building $f"
     go build $f
     bin=$(echo $f | sed 's/.go//' | xargs basename)
-    rm $bin
+    rm -f $bin
 done
 for f in *.go; do
     echo "==      building $f"
